197e5f0e910159e405b0e5250f2e19327eb165c0
MinhQuan2121 <106736476+MinhQuan2121@users.noreply.github.com>
2023-04-11 10:32:05 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,90 @@
+const toggler = document.querySelector('.toggler');
+const menu_toggler = document.querySelector('.menu-toggler');
+
+toggler.onclick = function(){
+    menu_toggler.classList.toggle('open');
+    toggler.classList.toggle('active');
+}
+
+const modalViews = document.querySelectorAll('.modal-content'),
+    modalBtns = document.querySelectorAll('.modalBtns'),
+    modalCloses = document.querySelectorAll('.modalCloses')
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
+
+const carousel = document.querySelector(".carousel"),
+    firstImg = carousel.querySelectorAll('img')[0];
+    arrowIcons = document.querySelectorAll(".wrapper i");
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+let firstImgWidth = firstImg.clientWidth * 14;
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let firstImgWidth = firstImg.clientWidth + 14;
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+    });
+});
+
+
+
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragStart) return;
+    e.preventDefault();
+    carousel.classList.add('dragging')
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = e.prevScrollLeft - positionDiff;
+}
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove('dragging');
+}
+
+carousel.addEventListener('mousedown', dragStart);
+carousel.addEventListener('mousemove', dragging);
+carousel.addEventListener('mouseup', dragStop);
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".header").style.top = "0";
+  } else {
+    document.querySelector(".header").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
